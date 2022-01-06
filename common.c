a8b3353cc77670ddfaf8f1708c333753d4e1ebf8
leticiasma <leti.ufmg@gmail.com>
2022-01-05 23:56:02 -0300

    Código base client e server

--- common.c
+++ common.c
@@ -1,7 +1,22 @@
-#include <inttypes.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+// #include <inttypes.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
 
-#include <arpa/inet.h>
+// #include <arpa/inet.h>
+
+#include "common.h"
+
+void DieWithUserMessage(const char *msg, const char *detail) {
+    fputs(msg, stderr);
+    fputs(": ", stderr);
+    fputs(detail, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
+
+void DieWithSystemMessage(const char *msg) {
+    perror(msg);
+    exit(1);
+}
 
--- common.h
+++ common.h
@@ -1,13 +1,23 @@
-#pragma once
+// #pragma once
 
-#include <stdlib.h>
+// #include <stdlib.h>
+// #include <stdio.h>
+// #include <string.h>
+// #include <unistd.h>
+// #include <stdbool.h>
+
+// #include <sys/types.h>
+// #include <sys/socket.h>
+// #include <arpa/inet.h>
+
+// #define BUFSZ 500
+
+
 #include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdbool.h>
+#include <stdlib.h>
 
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
+#define MAXSTRINGLENGTH 1024 //buff size
 
-#define BUFSZ 500
+void DieWithUserMessage(const char *msg, const char *detail);
+
+void DieWithSystemMessage(const char *msg);
